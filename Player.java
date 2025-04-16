76038dda873986a5360da92c3d47e5055a65e241
sherylwithab <sheryl.a.biju@gmail.com>
2025-04-16 16:40:21 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -6,6 +6,8 @@ public class Player {
     private boolean moreThan21 = false;
     private int sum;
     private boolean endOfTurn = false;
+    private int balance = 200;
+    private int bet;
     public Player(ArrayList<Card> hand) {
         this.hand = hand;
         this.sum = Deck.calculateHandValue(hand);
@@ -20,9 +22,19 @@ public class Player {
     public ArrayList<Card> getHand() {
         return this.hand;
     }
+    public void setBet(int bet) {
+        this.balance -= bet;
+        this.bet = bet;
+    }
+    public int getBet() {
+        return this.bet;
+    }
+    public int getBalance() {
+        return this.balance;
+    }
     public void showHand() {
         for (Card card: hand) {
-            System.out.print(card.toString());
+            System.out.print(card.toString() + " ");
         }
         System.out.println();
     }
@@ -31,5 +43,7 @@ public class Player {
     }
     public void finishTurn() {
         this.endOfTurn = true;
+        this.bet = 0;
     }
+
 }
